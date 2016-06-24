97b80fc27d495c60bc72626d519d525d43750143
Kris <krisrudolph@gmail.com>
2016-06-24 02:47:56 -0700

    Update UIScrollView+VGParallaxHeader.h
    lean down

--- VGParallaxHeader/UIScrollView+VGParallaxHeader.h
+++ VGParallaxHeader/UIScrollView+VGParallaxHeader.h
@@ -7,39 +7,10 @@
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger, VGParallaxHeaderMode) {
-    VGParallaxHeaderModeCenter = 0,
-    VGParallaxHeaderModeFill,
-    VGParallaxHeaderModeTop,
-    VGParallaxHeaderModeTopFill,
-};
-
-typedef NS_ENUM(NSInteger, VGParallaxHeaderStickyViewPosition) {
-    VGParallaxHeaderStickyViewPositionBottom = 0,
-    VGParallaxHeaderStickyViewPositionTop,
-};
-
-typedef NS_ENUM(NSInteger, VGParallaxHeaderShadowBehaviour) {
-    VGParallaxHeaderShadowBehaviourHidden = 0,
-    VGParallaxHeaderShadowBehaviourAppearing,
-    VGParallaxHeaderShadowBehaviourDisappearing,
-    VGParallaxHeaderShadowBehaviourAlways,
-} __deprecated;
-
 @interface VGParallaxHeader : UIView
-@property (nonatomic, assign, readonly) VGParallaxHeaderMode mode;
-
-@property (nonatomic, assign, readwrite) VGParallaxHeaderStickyViewPosition stickyViewPosition;
-@property (nonatomic, assign, readwrite) NSLayoutConstraint *stickyViewHeightConstraint;
-@property (nonatomic, strong, readwrite) UIView *stickyView;
 
 @property (nonatomic, assign, readonly, getter=isInsideTableView) BOOL insideTableView;
-@property (nonatomic, assign, readonly) CGFloat progress;
-
-@property (nonatomic, assign, readonly) VGParallaxHeaderShadowBehaviour shadowBehaviour __deprecated;
-
-- (void)setStickyView:(UIView *)stickyView
-           withHeight:(CGFloat)height;
+- (void)adjustHeightTo:(CGFloat)height animate:(BOOL)animate delay:(CGFloat)delay;
 
 @end
 
@@ -48,15 +19,7 @@ typedef NS_ENUM(NSInteger, VGParallaxHeaderShadowBehaviour) {
 @property (nonatomic, strong, readonly) VGParallaxHeader *parallaxHeader;
 
 - (void)setParallaxHeaderView:(UIView *)view
-                         mode:(VGParallaxHeaderMode)mode
                        height:(CGFloat)height;
-
-- (void)setParallaxHeaderView:(UIView *)view
-                         mode:(VGParallaxHeaderMode)mode
-                       height:(CGFloat)height
-              shadowBehaviour:(VGParallaxHeaderShadowBehaviour)shadowBehaviour __deprecated_msg("Use sticky view instead of shadow");
-
-- (void)updateParallaxHeaderViewHeight:(CGFloat)height;
 
 - (void)shouldPositionParallaxHeader;
 
